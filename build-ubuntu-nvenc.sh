e8edc79469152fe3f57515a2af57dbfa978fd920
Billy Riantono <bernadus@mail.unpas.ac.id>
2020-05-27 19:25:41 +0700

    Update build-ubuntu-nvenc.sh

--- build-ubuntu-nvenc.sh
+++ build-ubuntu-nvenc.sh
@@ -33,6 +33,10 @@ sudo apt-get -y --force-yes install \
 #nvenc dependencies
 sudo apt-get -y install glew-utils libglew-dev libglew2.0 freeglut3 freeglut3-dev libghc-glut-dev libghc-glut-doc libghc-glut-prof libalut-dev libxmu-dev libxmu-headers libxmu6 libxmu6-dbg libxmuu-dev libxmuu1 libxmuu1-dbg git-core
 
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
+sudo apt-get update
+
+sudo apt install nvidia-driver-440 xserver-xorg-video-nvidia-440 nvidia-utils-440 nvidia-kernel-source-440 nvidia-kernel-common-440 nvidia-dkms-440 nvidia-compute-utils-440 libnvidia-ifr1-440 libnvidia-gl-440 libnvidia-fbc1-440 libnvidia-encode-440 libnvidia-decode-440 libnvidia-cfg1-440 libnvidia-compute-440
 
 # For 12.04
 # libx265 requires cmake version >= 2.8.8
